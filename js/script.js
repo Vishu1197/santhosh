8b788629331db8910fe4a9e10d59beee40b103c6
Vishu1197 <vishalchanda.reva@gmail.com>
2026-04-07 02:22:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,7 @@
-gsap.registerPlugin(ScrollTrigger);
-
-/* PIN SCROLL (APPLE EFFECT) */
-const panels = gsap.utils.toArray(".panel");
-
-panels.forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "top top",
-    pin: true,
-    pinSpacing: false
-  });
-});
-
-/* TEXT REVEAL */
-gsap.utils.toArray(".panel h2, .panel p, .panel h1").forEach((el) => {
-  gsap.from(el, {
-    opacity: 0,
-    y: 80,
-    duration: 1,
-    scrollTrigger: {
-      trigger: el,
-      start: "top 80%"
-    }
-  });
-});
-
-/* EXPERIENCE SCROLL ANIMATION */
-gsap.utils.toArray(".exp div").forEach((el, i) => {
-  gsap.to(el, {
-    opacity: 1,
-    x: 50,
-    scrollTrigger: {
-      trigger: el,
-      start: "top 90%",
-      scrub: true
-    }
+document.querySelectorAll('a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
   });
 });
