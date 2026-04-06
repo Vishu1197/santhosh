c2b1020929cca046cf722525a3558e2ff53a50c1
Vishu1197 <vishalchanda.reva@gmail.com>
2026-04-06 22:08:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+// Typing animation
 const text = "Santhosh Viren Vijayan";
 let index = 0;
 
 function typeEffect() {
-    if(index < text.length){
+    if (index < text.length) {
         document.querySelector(".typing").innerHTML += text.charAt(index);
         index++;
         setTimeout(typeEffect, 80);
     }
 }
 typeEffect();
+
+// Scroll animation
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        if (position < window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
